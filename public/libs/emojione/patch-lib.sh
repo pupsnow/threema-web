ca8008c7e3be11da7d89e251b9752e2e1526cbdd
Danilo Bargen <danilo.bargen@threema.ch>
2017-05-04 11:58:47 +0200

    Update emoji picker for Emojione 3

--- public/libs/emojione/patch-lib.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-set -e
-sed -i "s/emojione-32-/e1-/g" emojione-sprite-32.min.css
-sed -i "s/emojione emojione-'+size+\"-\"/e1 e1-'/g" emojione.min.js
-sed -i "s/\"emojione/\"e1/g" emojione.min.js
